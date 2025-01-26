06ca50551e38dec4888489f33314b616ca8d4788
Varun kangotra <varunkangotra.it25@gmail.com>
2025-01-26 19:18:28 +0530

    New URL

--- routes/upload.js
+++ routes/upload.js
@@ -5,19 +5,11 @@ export async function upload(req, res){
     const file = req.file;
   
     if (file) {
-      const fileUrl = `https://filenow-production.up.railway.app/${file.filename}`;
-      const downloadUrl = `https://filenow-production.up.railway.app/${file.filename}`;
+      const fileUrl = `https://filenow-backend-production.up.railway.app/${file.filename}`;
+      const downloadUrl = `https://filenow-backend-production.up.railway.app/download/${file.filename}`;
       const formattedSize = formatFileSize(file.size);
-
-      const data = new fileModel({
-          file_name: file.filename,
-          file_size: formattedSize,
-          file_url: fileUrl,
-          download_url: downloadUrl
-      });
-      await data.save();
   
-      return res.status(200).json({
+      return res.status(201).json({
         message: "File uploaded successfully.",
         file_url: fileUrl,
         download_url: downloadUrl,
